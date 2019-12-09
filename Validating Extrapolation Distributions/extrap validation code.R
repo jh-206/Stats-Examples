1242d09fdac6e77a51bc13d616b5878d555bf0cb
jh-206 <jhirschi206@gmail.com>
2019-12-08 16:11:27 -0800

    Rename and init rmd

--- Validating Extrapolation Distributions/extrap validation code.R
+++ Validating Extrapolation Distributions/extrap validation code.R
@@ -3,7 +3,7 @@
 
   options(scipen = 999)
 
-  pacman::p_load(tidyverse)
+  pacman::p_load(tidyverse, ggridges)
 
 
 # Simulate Data -----------------------------------------------------------
@@ -12,28 +12,27 @@
 
   # Values tested for inflation
   df_true <- data.frame(
-    id = 1:100,
-    x = runif(100, min = 100000, max = 1000000)
+    x = 1:100
   )
 
   # Extrapolation Distributions
   ## Narrower Distribution
-  rnorm(1001, mean = df_true$x[1] / 1.08, sd = 50000)
+  # rnorm(1001, mean = df_true$x[1] / 1.08, sd = 50000)
   
-  l1 <- lapply(df_true$x, function(x) rnorm(1001, mean = x / 1.08, sd = 50000))
+  l1 <- lapply(df_true$x, function(x) rnorm(1001, mean = x, sd = 1))
   l1 <- lapply(l1, function(v) {names(v) = paste0('pred_', 1:length(v)); v})
   
   ## Wider Distribution
-  l2 <- lapply(df_true$x, function(x) rnorm(1001, mean = x / 1.08, sd = 100000))
+  l2 <- lapply(df_true$x, function(x) rnorm(1001, mean = x, sd = 1.5))
   l2 <- lapply(l2, function(v) {names(v) = paste0('pred_', 1:length(v)); v})
   
-  df = tibble(id = 1:100, dist1 = l1, dist2 = l2)
+  df = tibble(x = 1:100, dist1 = l1, dist2 = l2)
   
 
 # Bootstrap Methodology ---------------------------------------------------
 
   # Bootstrap sample of rows
-  inds <- sample(df_extrap1$id, nrow(df_extrap1), replace = T)
+  inds <- sample(df$x, nrow(df), replace = T)
   
   bootWrap <- function(x) {
     return(sample(x, length(x), replace = T))
@@ -48,3 +47,23 @@
            m1 = purrr::map_dbl(boot_dist1, mean),
            m2 = purrr::map_dbl(boot_dist2, mean))
     
+  
+  hist(df2$dist1[[1]])
+  
+  tit <- 'Plot Title'
+  ggplot(mapping = aes(df2$dist1[[1]])) + geom_histogram(alpha = .6, color = 'red',
+                                                         fill = 'red') + theme_bw() + labs(title = tit, x = 'X')
+  
+  xx = unnest(head(df, 12), cols = c(dist1, dist2))
+  
+  ggplot(xx, aes(x = dist1, y = as.factor(x))) +
+    geom_density_ridges() +
+    theme_ridges() + 
+    theme(legend.position = "none")
+  
+  ggplot(diamonds, aes(x = price, y = cut, fill = cut)) +
+    geom_density_ridges() +
+    theme_ridges() + 
+    theme(legend.position = "none")
+  
+  
